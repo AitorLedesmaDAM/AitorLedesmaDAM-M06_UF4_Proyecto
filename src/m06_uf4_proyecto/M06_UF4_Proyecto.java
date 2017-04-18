af97fc16ce54b74b447f04cf5c43b56d09a60489
AitorLedesmaDAM <aitorledesmadam@gmail.com>
2017-04-18 20:56:35 +0200

    Añadida la libreria

--- src/m06_uf4_proyecto/M06_UF4_Proyecto.java
+++ src/m06_uf4_proyecto/M06_UF4_Proyecto.java
@@ -15,7 +15,7 @@ public class M06_UF4_Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }
